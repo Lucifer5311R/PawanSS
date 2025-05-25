b6fe832dfa4413c8d2b7075ef155040ad62ddc8f
Durgadutt <durgaduttsb006@gmail.com>
2025-05-25 14:34:09 +0530

    added backend url

--- Art-of-Law/backend/server.js
+++ Art-of-Law/backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // CORS configuration (allow credentials if using cookies/sessions)
 app.use(cors({
-  origin: 'http://localhost:3000', // Your frontend URL
+  origin:[ 'http://localhost:3000','https://pawanss-artoflaw.vercel.app'], // Your frontend URL
   credentials: true // If you plan to use cookies or sessions for auth
 }));
 
